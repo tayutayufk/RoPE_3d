7885260448bd0c47774b366f86dd8e1c713a84ab
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-16 15:09:06 +0900

    WebGL板Unity

---
+++ button.js
@@ -0,0 +1,22 @@
+function onBB(){
+    gameInstance.SendMessage('GameDirector','BB');
+}
+function onMB(){
+    gameInstance.SendMessage('GameDirector','MB');
+}
+function onMN(){
+    gameInstance.SendMessage('GameDirector','MN');
+}
+function onMM(){
+    gameInstance.SendMessage('GameDirector','MM');
+}
+function onTM(){
+    gameInstance.SendMessage('GameDirector','TM');
+}
+
+function onSave(){
+    gameInstance.SendMessage('GameDirector','SaveJson');
+}
+function onLoad(){
+    gameInstance.SendMessage('GameDirector','LoadJson');
+}
